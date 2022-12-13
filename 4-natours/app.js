62d72d34b9a8706e57eb39110e590fc5614ead03
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-12-13 16:45:32 +0300

    Natours Project

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json())
 
 // app.get('/',(req, res)=>{
 //     res.status(404).json({message:"Hello from the server side", app: "Natours"});
@@ -9,8 +11,69 @@ const app = express();
 //     res.send('You can post to this endpoint...')
 // })
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 app.get('/api/v1/tours',(req, res)=>{   // Callback function here is called route handler
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+})
 
+app.get('/api/v1/tours/:id',(req,res) =>{
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+    if(id < tours.length)
+    {
+        res.status(200).json({
+            status : "success",
+            data:{
+                tour
+            }
+        })
+    }
+    else
+    {
+    res.status(404).json({
+        status: "fail",
+        message: "Invalid ID"
+    })
+}
+    
+
+
+
+
+//     else
+//     {
+//     res.status(200).json({
+//         status : "success",
+//         data:{
+//             tour
+//         }
+//     })
+// }
+})
+
+app.post('/api/v1/tours',(req, res)=>{
+    // console.log(req.body);
+    const newId = tours[tours.length - 1].id  + 1;
+    const newTour = Object.assign({id: newId},req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err =>{
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+    }
+
+    )
+    
 })
 
 const port = 3000;
