30bb666d1e3a1eda631b3605ae32890a8b377c07
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-12-16 03:24:09 +0300

    Natours Project

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'});
 const app = require('./app');
 
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
 //  Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`);
-})
+});
+
