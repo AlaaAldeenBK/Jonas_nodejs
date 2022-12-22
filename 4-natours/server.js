c363b5a013210d61db799de27d726a929e0cfa2b
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-12-22 22:21:05 +0300

    Natour Project

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -5,7 +5,6 @@ dotenv.config({ path: './config.env' });
 // console.log(app.get('env'));
 // console.log(process.env);
 
-
 //  Start  server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
