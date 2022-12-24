91b835ed43b88f812e5d7bf31833fcce86d7f52e
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-12-24 17:16:18 +0300

    Natour Project

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController')
+const tourController = require('./../controllers/tourController');
 
 // Routes
 const router = express.Router();
-router.param('id',tourController.checkID)
+router.param('id', tourController.checkID);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkID, tourController.createTour);
 
 router
-.route('/')
-.get(tourController.getAllTours)
-.post(tourController.c, tourController.createTour);
-
-router
-.route('/:id')
-.get(tourController.getTour)
-.patch(tourController.updateTour)
-.delete(tourController.deleteTour);
-
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 module.exports = router;
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 dotenv.config({ path: './config.env' });
+// console.log(process.env.NODE-ENV);
+// dotenv.config({ path: path.resolve(__dirname, './config.env') })
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con =>{
+  console.log(con.connections)
+  console.log("DB connection successfully")
+})
 
 // console.log(app.get('env'));
 // console.log(process.env);
